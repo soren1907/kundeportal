c52307d871b466644c6e0c104b114ea79d5c4cb9
soren1907 <sorenraben@hotmail.com>
2021-12-27 17:29:12 +0100

    deleted css

--- public/user_pages/user_session.js
+++ public/user_pages/user_session.js
@@ -1,6 +1,4 @@
 //Method check if there is a session(first time visit) -> show cookies.
-//Checks if a user is logged in. 
-//if logged in, goes to user-profile and dont show login page.
 //Script is called in footer.html (will be used on every route)
  
 fetch("/api/new_session_check").then(res => {
